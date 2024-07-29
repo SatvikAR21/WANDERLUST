cab078757f04e6bcdfc562386e48a9d8aa810953
SatvikAR21 <satvikgupta212003@gmail.com>
2024-07-29 10:34:00 +0530

    fixed error

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 const Listing = require("./models/listing.js");
-const ExpressError = require("./utils/Expresserror.js");
+const ExpressError = require("./utils/ExpressError.js");
 const {listingSchema,reviewSchema}=require("./schema.js");
 const Review = require("./models/review");
 const mongoose = require("mongoose");
